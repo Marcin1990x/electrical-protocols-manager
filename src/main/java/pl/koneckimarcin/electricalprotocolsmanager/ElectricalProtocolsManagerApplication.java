80b6e87888cad1d654e40a23996342f4153a2ec7
Marcin1990x <marcin.konecki21@gmail.com>
2023-09-14 19:18:31 +0200

    First commit.

---
+++ src/main/java/pl/koneckimarcin/electricalprotocolsmanager/ElectricalProtocolsManagerApplication.java
@@ -0,0 +1,13 @@
+package pl.koneckimarcin.electricalprotocolsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricalProtocolsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectricalProtocolsManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/koneckimarcin/electricalprotocolsmanager/ElectricalProtocolsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.koneckimarcin.electricalprotocolsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectricalProtocolsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
