6a41707ae4ada174de348eb19173172358499b51
Marcin1990x <marcin.konecki21@gmail.com>
2023-09-23 18:20:32 +0200

    PdfMeasurementDataService - add method to append measurement data for each page of document.

--- src/main/java/pl/koneckimarcin/electricalprotocolsmanager/pdf/service/PdfMeasurementDataService.java
+++ src/main/java/pl/koneckimarcin/electricalprotocolsmanager/pdf/service/PdfMeasurementDataService.java
@@ -1,26 +1,48 @@
 package pl.koneckimarcin.electricalprotocolsmanager.pdf.service;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.apache.pdfbox.pdmodel.font.Standard14Fonts;
-import pl.koneckimarcin.electricalprotocolsmanager.pdf.model.PdfMeasurementsData;
+import org.springframework.stereotype.Service;
+import pl.koneckimarcin.electricalprotocolsmanager.measurement.MeasurementMain;
+import pl.koneckimarcin.electricalprotocolsmanager.structure.model.Building;
 
 import java.io.IOException;
+import java.util.List;
 
+@Service
 public class PdfMeasurementDataService {
 
-    public static void addMeasurementData(PDDocument document, PDPage page, PdfMeasurementsData measurementsData) throws IOException {
+    private final int headingXposition = 50;
+    private final int headingYposition = 680;
+    private final int headingLeading = 10;
+    private final int headingFontSize = 10;
+    private final PDFont font = new PDType1Font(Standard14Fonts.FontName.COURIER);
 
-        PDFont pdFont = new PDType1Font(Standard14Fonts.FontName.COURIER);
+    private PdfTextService textService;
 
-        PDPageContentStream content = new PDPageContentStream(document, page);
+    public PdfMeasurementDataService(PdfTextService textService) {
+        this.textService = textService;
+    }
 
-        PdfTextService.addMultipleLineOfText(content, measurementsData.getMeasurementData(),
-                50, 600, 10, pdFont, 10);
+    public void addMeasurementData(PDDocument document, Building building, int measurementPagesCount) throws IOException {
 
-        content.close();
+        PDPageContentStream content;
+
+        List<MeasurementMain> measurementMainList = building.getMeasurementMainList();
+
+        for(int i = 0; i < measurementPagesCount; i++){
+
+            content = new PDPageContentStream(document, document.getPage(i),
+                    PDPageContentStream.AppendMode.APPEND, false);
+
+            textService.addMultipleLineOfText(content, measurementMainList.get(i).getMeasurementsMainTextData(),
+                    headingXposition, headingYposition, headingLeading,
+                    font, headingFontSize);
+
+            content.close();
+        }
     }
 }
