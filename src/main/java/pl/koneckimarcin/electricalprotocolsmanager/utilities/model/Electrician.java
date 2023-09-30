b64b06e9fd5eb956ead750708f589dd61594b2ea
Marcin1990x <marcin.konecki21@gmail.com>
2023-09-30 12:23:42 +0200

    Electrician - rebuild class. Added few fields, created method to get signature. Added Position enum for Electrician.

--- src/main/java/pl/koneckimarcin/electricalprotocolsmanager/utilities/model/Electrician.java
+++ src/main/java/pl/koneckimarcin/electricalprotocolsmanager/utilities/model/Electrician.java
@@ -1,13 +1,26 @@
 package pl.koneckimarcin.electricalprotocolsmanager.utilities.model;
 
+import pl.koneckimarcin.electricalprotocolsmanager.measurement.data.TextData;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Electrician {
 
     private String firstName;
     private String lastName;
+    private String electricianAddress;
+    private List<String> permissionList;
+    private Position position;
+    private List<String> signature;
 
-    public Electrician(String firstName, String lastName) {
+    public Electrician(String firstName, String lastName, String electricianAddress, List<String> permissionList, Position position) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.electricianAddress = electricianAddress;
+        this.permissionList = permissionList;
+        this.position = position;
+        this.signature = addSignature();
     }
 
     public String getFirstName() {
@@ -26,8 +39,27 @@ public class Electrician {
         this.lastName = lastName;
     }
 
+    public List<String> getSignature() {
+        return signature;
+    }
+
     @Override
     public String toString() {
         return firstName + " " + lastName;
+    }
+
+    private List<String> addSignature() {
+
+        List<String> signature = new ArrayList<>();
+        signature.add(this.toString());
+        signature.add(TextData.electriciansPageText.get(0));
+        signature.add(TextData.electriciansPageText.get(1));
+        String permissions = "";
+        for(String permission : this.permissionList) {
+            permissions = permissions + permission + " ";
+        }
+        signature.add(permissions);
+
+        return signature;
     }
 }
---
+++ src/main/java/pl/koneckimarcin/electricalprotocolsmanager/utilities/model/Position.java
@@ -0,0 +1,6 @@
+package pl.koneckimarcin.electricalprotocolsmanager.utilities.model;
+
+public enum Position {
+
+    MEASURER, CHECKER;
+}
