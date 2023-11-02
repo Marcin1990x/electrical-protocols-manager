7b430629d8392f459ea9582e374987f6736571a8
Marcin1990x <marcingothico14>
2023-11-02 16:55:55 +0100

    Main Controller - added endpoint to retrieve all main measurements.

--- src/main/java/pl/koneckimarcin/electricalprotocolsmanager/measurement/main/MainController.java
+++ src/main/java/pl/koneckimarcin/electricalprotocolsmanager/measurement/main/MainController.java
@@ -1,16 +1,30 @@
 package pl.koneckimarcin.electricalprotocolsmanager.measurement.main;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import pl.koneckimarcin.electricalprotocolsmanager.measurement.data.TextData;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class MainController {
 
+    private MeasurementMainRepository repository;
+
+    public MainController(MeasurementMainRepository repository) {
+        this.repository = repository;
+    }
+
     @GetMapping("/types")
     public List<String> retrieveMeasurementMainTypes() {
         return TextData.measurementsMainNames;
+    }
+
+    @GetMapping("/measurements/{id}")
+    public MeasurementMain getMeasurementMainById(@PathVariable int id) {
+        Optional<MeasurementMain> main = repository.findById(id);
+        return main.get();
     }
 }
